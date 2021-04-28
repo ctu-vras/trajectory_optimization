fixed bugs with smooth visibility masks calculation

src/model.py
@@ -6,6 +6,7 @@ from tools import load_intrinsics, hidden_pts_removal
 import torch.nn.functional as F
 from copy import deepcopy
 from time import time
+import rospy
 
 
 # Helper functions
@@ -17,48 +18,38 @@ def get_dist_mask(points, min_dist=1.0, max_dist=5.0):
 
     # smooth and differentiable implementation of the above logic
     mean = torch.tensor((min_dist + max_dist) / 2.)
-    std = torch.tensor(max_dist - min_dist)
+    std = torch.tensor((max_dist - min_dist) / 2.)
     dist = torch.linalg.norm(points - mean, dim=1)
     dist_mask = torch.exp(-0.5 * (dist / std) ** 2)
     return dist_mask
 
 
-def get_fov_mask(points, img_height, img_width, intrins):
+def get_fov_mask(points, img_height, img_width, intrins, eps=1e-6, binary=False):
     # find points that are observed by the camera (in its FOV)
     assert isinstance(points, torch.Tensor)
     assert points.size()[1] == 3  # points.size() = N x 3
     assert isinstance(intrins, torch.Tensor)
     assert intrins.size() == torch.Size([3, 3])
 
-    # pts_homo = torch.matmul(intrins, torch.transpose(points, 0, 1))
-    # pts_homo[:2] /= pts_homo[2]
-    # fov_mask = (pts_homo[2] > 0) & \
-    #            (pts_homo[0] > 1) & (pts_homo[0] < img_width - 1) & \
-    #            (pts_homo[1] > 1) & (pts_homo[1] < img_height - 1)
-
-    # smooth and differentiable implementation of the above logic
-    pts_homo = torch.matmul(intrins, torch.transpose(points, 0, 1))
-    depth_tanh = torch.tanh(pts_homo[2])
-    width_gaussian = torch.exp(-0.5*((pts_homo[0]/pts_homo[2] - img_width/2) / img_width)**2)
-    height_gaussian = torch.exp(-0.5 * ((pts_homo[1]/pts_homo[2] - img_height/2) / img_height) ** 2)
-
-    fov_mask = depth_tanh * width_gaussian * height_gaussian
+    if binary:
+        pts_homo = torch.matmul(intrins, torch.transpose(points, 0, 1))
+        pts_homo[:2] /= pts_homo[2]
+        fov_mask = (pts_homo[2] > 0) & \
+                   (pts_homo[0] > 1) & (pts_homo[0] < img_width - 1) & \
+                   (pts_homo[1] > 1) & (pts_homo[1] < img_height - 1)
+    else:
+        # smooth implementation of the above logic
+        pts_homo = torch.matmul(intrins, torch.transpose(points, 0, 1))
+        depth_sigmoid = torch.sigmoid(pts_homo[2])
+        width_gaussian = torch.exp(-0.5 * ((pts_homo[0] / (pts_homo[2]+eps) - img_width / 2.) / img_width) ** 2)
+        height_gaussian = torch.exp(-0.5 * ((pts_homo[1] / (pts_homo[2]+eps) - img_height / 2.) / img_height) ** 2)
+        fov_mask = depth_sigmoid * width_gaussian * height_gaussian
     return fov_mask
 
 
-def visibility_estimation(points, img_height, img_width, intrins, pc_clip_limits, hpr=False):
-    dist_mask = get_dist_mask(points, pc_clip_limits[0], pc_clip_limits[1])
-    fov_mask = get_fov_mask(points, img_height, img_width, intrins)
-    if hpr:
-        # HPR: remove occluded points
-        occlusion_mask = hidden_pts_removal(verts.detach(), device=self.device)[1]
-        mask = occlusion_mask * (dist_mask * fov_mask)
-    else:
-        mask = dist_mask * fov_mask
-    return mask
-
-
 def to_camera_frame(verts, quat, trans):
+    assert verts.dim() == trans.dim()
+    assert quat.size() == torch.Size([1, 4])
     q = F.normalize(quat)
     q_inv = quaternion_invert(q)
     verts = verts - trans
@@ -73,14 +64,18 @@ Model for a single pose optimization
 
 class ModelPose(nn.Module):
     def __init__(self,
-                 points,
-                 trans0,  # t = (x, y, z), example: torch.tensor([[0., 0., 0.]])
-                 q0,  # q = (w, x, y, z), example: torch.tensor([[1., 0., 0., 0.]])
-                 intrins,  # torch.tensor, size=(3, 3)
+                 points: torch.tensor,
+                 trans0: torch.tensor,  # t = (x, y, z), example: torch.tensor([[0., 0., 0.]])
+                 q0: torch.tensor,  # q = (w, x, y, z), example: torch.tensor([[1., 0., 0., 0.]])
+                 intrins: torch.tensor,  # torch.tensor, size=(3, 3)
                  img_width, img_height,
                  min_dist=1.0, max_dist=5.0,
                  device=torch.device('cuda:0')):
         super().__init__()
+        assert trans0.size() == torch.Size([1, 3])
+        assert q0.size() == torch.Size([1, 4])
+        assert intrins.size() == torch.Size([3, 3])
+
         self.device = device
         self.points = points.to(self.device)
         self.rewards = None
@@ -100,27 +95,30 @@ class ModelPose(nn.Module):
 
         self.to(self.device)
 
-    def forward(self, hpr=False, debug=False):
+    def forward(self, debug=False, hpr=False):
         # transform points to camera frame
         t0 = time()
         points = to_camera_frame(self.points, self.quat, self.trans)
         if debug:
             print(f'\nPoint cloud transformation took: {1000*(time() - t0)} msec')
             print(f'Point cloud size {self.points.size()}')
-
         # get masks of points that are inside of the camera FOV
         t1 = time()
-        self.observations = visibility_estimation(points,
-                                                  self.img_height, self.img_width,
-                                                  self.K, self.pc_clip_limits)
+
+        dist_mask = get_dist_mask(points, self.pc_clip_limits[0], self.pc_clip_limits[1])
+        fov_mask = get_fov_mask(points, self.img_height, self.img_width, self.K, eps=self.eps)
+        mask = dist_mask * fov_mask
+
+        if hpr:
+            # HPR: remove occluded points
+            occlusion_mask = hidden_pts_removal(self.points.detach(), device=self.device)[1]
+            mask = occlusion_mask * mask
+
+        self.observations = mask
 
         if debug:
             print(f'Visibility estimation took: {1000 * (time() - t1)} msec')
-
-        t2 = time()
         loss = self.criterion(self.observations)
-        if debug:
-            print(f'Loss calculation took: {1000 * (time() - t2)} msec \n')
         return loss
 
     def criterion(self, observations):
@@ -160,14 +158,18 @@ def mean_angle_calc(traj_wps, eps=1e-6):
 class ModelTraj(nn.Module):
     def __init__(self,
                  points: torch.tensor,
-                 wps_poses: torch.tensor,  # (N, 1, 3): [[[x0, y0, z0]], [[x1, y1, z1]], ...]
-                 wps_quats: torch.tensor,  # (N,, 1, 4): torch.tensor: [w, x, y, z]-format
+                 wps_poses: torch.tensor,  # (N, 3): [[[x0, y0, z0]], [[x1, y1, z1]], ...]
+                 wps_quats: torch.tensor,  # (N, 4): torch.tensor: [w, x, y, z]-format
                  intrins: torch.tensor,  # torch.tensor, size=(3, 3)
                  img_width, img_height,
                  min_dist=1.0, max_dist=5.0,
                  smoothness_weight=14.0, traj_length_weight=0.02,
                  device=torch.device('cuda')):
         super().__init__()
+        assert wps_poses.dim() == wps_quats.dim()
+        assert wps_poses.size()[1] == 3
+        assert wps_quats.size()[1] == 4
+
         self.device = device
         self.points = torch.as_tensor(points, dtype=torch.float32).to(self.device)
         self.rewards = None
@@ -195,7 +197,9 @@ class ModelTraj(nn.Module):
 
         self.to(self.device)
 
-    def forward(self, debug=False):
+    def forward(self,
+                vis_wps_dist=0.5,  # distance between neighbor waypoints to estimate visibility
+                debug=False):
         """
         Trajectory evaluation based on visibility estimation from its waypoints.
         traj_score = log_odds_sum([visibility_estimation(wp) for wp in traj_waypoints])
@@ -204,16 +208,26 @@ class ModelTraj(nn.Module):
         N_wps = len(self.poses)
         lo_sum = 0.0
         # TODO: replace for loop with tensors operations in order not to calculate rewards consequently for waypoints
-        for i in range(N_wps):
+
+        # choose waypoints to compute visibility at
+        # based on mean waypoints distance in the initial trajectory
+        mean_wps_dist = (self.poses0[1:, :] - self.poses0[:-1, :]).norm(dim=1).mean()
+        wps_step = int(vis_wps_dist / mean_wps_dist) + 1
+        # rospy.loginfo(f'Estimating visibility at every {wps_step} waypoint')
+        for i in range(0, N_wps, wps_step):
             # transform points to camera frame
             points = to_camera_frame(self.points, self.quats[i].unsqueeze(0), self.poses[i].unsqueeze(0))
 
             dist_mask = get_dist_mask(points, self.pc_clip_limits[0], self.pc_clip_limits[1])
-            # fov_mask = get_fov_mask(points, self.img_height, self.img_width, self.K)
-            p = dist_mask #* fov_mask
+            fov_mask = get_fov_mask(points, self.img_height, self.img_width, self.K, eps=self.eps)
+            p = dist_mask * fov_mask
+
+            # normalize observations in (0, 1) probabilities interval
+            p = p - p.min()
+            p = p / p.max()
             # apply log odds conversion for global voxel map observations update
-            p = torch.clip(p, 0.5, 1.0 - self.eps)
-            lo = torch.log(p / (1.0 - p))
+            p = torch.clip(p, 0.5, 1. - self.eps)
+            lo = torch.log(p / (1. - p))
             lo_sum = lo_sum + lo
 
         if debug:
@@ -229,7 +243,7 @@ class ModelTraj(nn.Module):
 
     def criterion(self, rewards):
         # transform observations to loss function: loss = 1 / mean(prob(observed))
-        self.loss['vis'] = 1.0 / (torch.mean(rewards) + self.eps)
+        self.loss['vis'] = 1. / (torch.mean(rewards) + self.eps)
 
         # penalties for being far from initial waypoints
         self.loss['l2'] = torch.linalg.norm(self.poses[0] - self.poses0[0])

src/pose_publisher.py
@@ -13,21 +13,19 @@ import tf
 
 if __name__ == "__main__":
     rospy.init_node('pose_publisher')
-    # Define initial position to optimize
-    trans = np.array([rospy.get_param('pose_publisher/x', 6.0),
-                      rospy.get_param('pose_publisher/y', 2.0),
-                      rospy.get_param('pose_publisher/z', 0.0)])
-
-    quat_xyzw = tf.transformations.quaternion_from_euler(
-                      rospy.get_param('pose_publisher/roll', 0.0),
-                      rospy.get_param('pose_publisher/pitch', 0.0),
-                      rospy.get_param('pose_publisher/yaw', 0.0))
-
-    # Run optimization loop
     rate = rospy.Rate(rospy.get_param('pose_publisher/rate', 1))
     while True:
         if rospy.is_shutdown():
             break
+        # Define initial position to optimize
+        trans = np.array([rospy.get_param('pose_publisher/x', np.random.random() * 5 + 15),
+                          rospy.get_param('pose_publisher/y', np.random.random() * 5 + 15),
+                          rospy.get_param('pose_publisher/z', np.random.random() * 2)])
+
+        quat_xyzw = tf.transformations.quaternion_from_euler(
+            rospy.get_param('pose_publisher/roll', np.random.random() * np.pi),
+            rospy.get_param('pose_publisher/pitch', np.random.random() * np.pi),
+            rospy.get_param('pose_publisher/yaw', np.random.random() * np.pi))
 
         publish_pose(trans, quat_xyzw, rospy.get_param('pose_publisher/output_topic', '/pose'), rospy.Time.now(), frame_id='world')
         rate.sleep()

src/tools.py
@@ -204,17 +204,19 @@ def publish_image(img, topic='/image/compressed'):
     pub.publish(img_msg)
 
 
-def publish_odom(pose, orient, frame='/odom', topic='/odom_0'):
+def publish_odom(pose, quat, frame='/odom', topic='/odom_0'):
+    assert len(pose) == 3
+    assert len(quat) == 4
     odom_msg_0 = Odometry()
     odom_msg_0.header.stamp = rospy.Time.now()
     odom_msg_0.header.frame_id = frame
     odom_msg_0.pose.pose.position.x = pose[0]
     odom_msg_0.pose.pose.position.y = pose[1]
     odom_msg_0.pose.pose.position.z = pose[2]
-    odom_msg_0.pose.pose.orientation.x = orient[0]
-    odom_msg_0.pose.pose.orientation.y = orient[1]
-    odom_msg_0.pose.pose.orientation.z = orient[2]
-    odom_msg_0.pose.pose.orientation.w = orient[3]
+    odom_msg_0.pose.pose.orientation.x = quat[0]
+    odom_msg_0.pose.pose.orientation.y = quat[1]
+    odom_msg_0.pose.pose.orientation.z = quat[2]
+    odom_msg_0.pose.pose.orientation.w = quat[3]
     pub = rospy.Publisher(topic, Odometry, queue_size=1)
     pub.publish(odom_msg_0)
 
@@ -229,7 +231,9 @@ def publish_pointcloud(points, topic_name, stamp, frame_id):
     pub.publish(pc_msg)
 
 
-def publish_tf_pose(pose, orient, child_frame_id, frame_id="world"):
+def publish_tf_pose(pose, quat, child_frame_id, frame_id="world"):
+    assert len(pose) == 3
+    assert len(quat) == 4
     br = tf2_ros.TransformBroadcaster()
     t = TransformStamped()
     t.header.stamp = rospy.Time.now()
@@ -238,10 +242,10 @@ def publish_tf_pose(pose, orient, child_frame_id, frame_id="world"):
     t.transform.translation.x = pose[0]
     t.transform.translation.y = pose[1]
     t.transform.translation.z = pose[2]
-    t.transform.rotation.x = orient[0]
-    t.transform.rotation.y = orient[1]
-    t.transform.rotation.z = orient[2]
-    t.transform.rotation.w = orient[3]
+    t.transform.rotation.x = quat[0]
+    t.transform.rotation.y = quat[1]
+    t.transform.rotation.z = quat[2]
+    t.transform.rotation.w = quat[3]
     br.sendTransform(t)
 
 
@@ -267,7 +271,9 @@ def publish_camera_info(image_width=1232, image_height=1616,
     pub.publish(camera_info_msg)
 
 
-def to_pose_stamped(pose, orient, stamp=None, frame_id='world'):
+def to_pose_stamped(pose, quat, stamp=None, frame_id='world'):
+    assert len(pose) == 3
+    assert len(quat) == 4
     msg = PoseStamped()
     msg.header.seq = 0
     if stamp is None:
@@ -277,18 +283,19 @@ def to_pose_stamped(pose, orient, stamp=None, frame_id='world'):
     msg.pose.position.x = pose[0]
     msg.pose.position.y = pose[1]
     msg.pose.position.z = pose[2]
-    quaternion = tf.transformations.quaternion_from_euler(orient[0], orient[1], orient[2])
-    msg.pose.orientation.x = quaternion[0]
-    msg.pose.orientation.y = quaternion[1]
-    msg.pose.orientation.z = quaternion[2]
-    msg.pose.orientation.w = quaternion[3]
+    msg.pose.orientation.x = quat[0]
+    msg.pose.orientation.y = quat[1]
+    msg.pose.orientation.z = quat[2]
+    msg.pose.orientation.w = quat[3]
     msg.header.seq += 1
     msg.header.stamp = rospy.Time.now()
     return msg
 
 
-def publish_pose(pose, orient, topic_name, stamp=None, frame_id='world'):
-    msg = to_pose_stamped(pose, orient, stamp=stamp, frame_id=frame_id)
+def publish_pose(pose, quat, topic_name, stamp=None, frame_id='world'):
+    assert len(pose) == 3
+    assert len(quat) == 4
+    msg = to_pose_stamped(pose, quat, stamp=stamp, frame_id=frame_id)
     pub = rospy.Publisher(topic_name, PoseStamped, queue_size=1)
     pub.publish(msg)
 

src/trajectory_optimization_sample.py
@@ -43,10 +43,10 @@ def load_data(index=None):
     poses_np = np.load(poses_filename)['poses']
 
     # orientations: quaternion for each waypoint
-    xyzw = tf.transformations.quaternion_from_euler(0.0, 0.0, 0.0)
-    quats_wxyz_np = np.asarray([[xyzw[3], xyzw[0], xyzw[1], xyzw[2]]], dtype=np.float32)
+    x, y, z, w = tf.transformations.quaternion_from_euler(0.0, 0.0, 0.0)
+    quats_wxyz_np = np.asarray([[w, x, y, z]], dtype=np.float32)
     for _ in range(len(poses_np) - 1):
-        quats_wxyz_np = np.vstack([quats_wxyz_np, np.asarray([[xyzw[3], xyzw[0], xyzw[1], xyzw[2]]], dtype=np.float32)])
+        quats_wxyz_np = np.vstack([quats_wxyz_np, np.asarray([[w, x, y, z]], dtype=np.float32)])
     return pts_np, poses_np, quats_wxyz_np
 
 
@@ -56,7 +56,7 @@ N_steps = rospy.get_param('traj_opt/opt_steps', 400)
 smooth_weight = rospy.get_param('traj_opt/smooth_weight', 14.0)
 length_weight = rospy.get_param('traj_opt/length_weight', 0.02)
 lr_pose = rospy.get_param('traj_opt/lr_pose', 0.1)
-lr_quat = rospy.get_param('traj_opt/lr_quat', 0.0)
+lr_quat = rospy.get_param('traj_opt/lr_quat', 0.02)
 
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     rospy.init_node('camera_traj_optimization')
 
     # Load the point cloud and initial trajectory to optimize
-    index = 3  # np.random.choice(range(0, 15))  # 0-98 or None - for random
+    index = np.random.choice(range(0, 15))  # 0-98 or None - for random
     pts_np, poses_np, quats_wxyz_np = load_data(index=index)
 
     points = torch.tensor(pts_np, dtype=torch.float32).to(device)
@@ -99,7 +99,7 @@ if __name__ == "__main__":
     smooth_loss0 = None
     OPTIMIZATION_COMPLETE = False
     N_optimal = N_steps
-    REWARDS_TH = 1.01
+    REWARDS_TH = 1.1
     SMOOTHNESS_TH = 0.9
 
     t_step = 0.0
@@ -130,38 +130,37 @@ if __name__ == "__main__":
         if i % pub_sample == 0:
             t2 = time()
             # debug = True
-            # if debug:
-            #     print(f'Gradient backprop took: {1000 * (time() - t1)} msec')
-            # for key in model.loss:
-            #     print(f"{key} loss: {model.loss[key]}")
+            if debug:
+                print(f'Gradient backprop took: {1000 * (time() - t1)} msec')
+
+                plt.cla()
+                plt.subplot(1,2,1)
+                # plt.grid()
+                plt.title('Visibility reward gain: R / R0')
+                plt.ylabel('R / R0')
+                plt.xlabel('opt steps')
+                plt.plot(log['visibility'], color='b')
+                if OPTIMIZATION_COMPLETE:
+                    plt.axvline(N_optimal, 0, 1)
+
+                plt.subplot(1,2,2)
+                # plt.grid()
+                plt.title('Trajectory smoothness')
+                plt.ylabel('Loss_{smooth}0 / Loss_{smooth}')
+                plt.xlabel('opt steps')
+                plt.plot(log['smoothness'], color='b')
+                if OPTIMIZATION_COMPLETE:
+                    plt.axvline(N_optimal, 0, 1)
+                plt.pause(0.01)
+                plt.draw()
+
             if FIRST_RECORD:
                 reward0 = torch.mean(model.rewards)
                 smooth_loss0 = model.loss['smooth']
                 FIRST_RECORD = False
-            # print(f"Trajectory visibility score: {torch.mean(model.rewards) / reward0}")
 
             log['visibility'].append(torch.mean(model.rewards) / reward0)
             log['smoothness'].append(smooth_loss0 / model.loss['smooth'])
-            # plt.cla()
-            # plt.subplot(1,2,1)
-            # # plt.grid()
-            # plt.title('Visibility reward gain: R / R0')
-            # plt.ylabel('R / R0')
-            # plt.xlabel('opt steps')
-            # plt.plot(log['visibility'], color='b')
-            # if OPTIMIZATION_COMPLETE:
-            #     plt.axvline(N_optimal, 0, 1)
-            #
-            # plt.subplot(1,2,2)
-            # # plt.grid()
-            # plt.title('Trajectory smoothness')
-            # plt.ylabel('Loss_{smooth}0 / Loss_{smooth}')
-            # plt.xlabel('opt steps')
-            # plt.plot(log['smoothness'], color='b')
-            # if OPTIMIZATION_COMPLETE:
-            #     plt.axvline(N_optimal, 0, 1)
-            # plt.pause(0.01)
-            # plt.draw()
 
             if not OPTIMIZATION_COMPLETE and \
                    log['visibility'][-1] > REWARDS_TH and \